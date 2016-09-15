8760612781db6f9207a33bd06378a0fb93ebc407
Anand <gauravanand0902@gmail.com>
2016-09-15 14:17:21 +0530

    TO DO FRAGMENT

--- app/src/main/java/androidapplication/com/todoapp/MainActivity.java
+++ app/src/main/java/androidapplication/com/todoapp/MainActivity.java
@@ -1,18 +1,45 @@
 package androidapplication.com.todoapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import androidapplication.com.todoapp.fragment.StaticsFragment;
+import androidapplication.com.todoapp.fragment.ToDoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonOne;
-
-
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonOne = (Button)findViewById(R.id.buttonOne);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
     }
+
+    public void selectFrag(View view){
+        Fragment fr;
+        if (view ==findViewById(R.id.button1)){
+            fr = new ToDoFragment();
+        }
+        else {
+            fr = new StaticsFragment();
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place,fr);
+        fragmentTransaction.commit();
+        drawerLayout.closeDrawers();
+
+    }
+
 }
