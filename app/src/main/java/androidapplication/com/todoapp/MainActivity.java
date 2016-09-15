f311d46457e50d91ea47804375aec925f5e530d5
Anand <gauravanand0902@gmail.com>
2016-09-15 23:15:19 +0530

    checkbox

--- app/src/main/java/androidapplication/com/todoapp/MainActivity.java
+++ app/src/main/java/androidapplication/com/todoapp/MainActivity.java
@@ -6,39 +6,64 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidapplication.com.todoapp.fragment.StaticsFragment;
 import androidapplication.com.todoapp.fragment.ToDoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    ArrayAdapter<String> listAdapter;
+    String fragmentArray[] = {"TO DO FRAGMENT", "STATICS"};
     DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
-    }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 
-    public void selectFrag(View view){
-        Fragment fr;
-        if (view ==findViewById(R.id.button1)){
-            fr = new ToDoFragment();
-        }
-        else {
-            fr = new StaticsFragment();
-        }
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_place,fr);
-        fragmentTransaction.commit();
-        drawerLayout.closeDrawers();
+        listView = (ListView) findViewById(R.id.fragmentList);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fragmentArray);
+        listView.setAdapter(listAdapter);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Fragment fragment;
+
+                switch (i) {
+                    case 0:
+                        fragment = new ToDoFragment();
+                        break;
+                    case 1:
+                        fragment = new StaticsFragment();
+                        break;
+                    default:
+                        fragment = new ToDoFragment();
+                        break;
+                }
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.relativeLayout, fragment).commit();
+                drawerLayout.closeDrawers();
+            }
+
+        });
+
+        Fragment fragment = new ToDoFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.relativeLayout, fragment).commit();
 
     }
 
